66f4200ce1b99c6f24e3649992546b31e903cfd5
Acácio Veit Schneider <acacio@spacerock.dev>
2020-12-18 09:50:55 -0300

    Added city entity

---
+++ lib/features/weather/domain/entities/city.dart
@@ -0,0 +1,13 @@
+import 'package:meta/meta.dart';
+
+class City {
+  final String name;
+  final String stateCode;
+  final String countryCode;
+
+  City({
+    @required this.name,
+    @required this.stateCode,
+    @required this.countryCode,
+  });
+}
