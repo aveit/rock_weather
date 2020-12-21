2e0b8d7c3a9b5a6b56f3f2f221f511fd70117f4a
Acácio Veit Schneider <acacio@spacerock.dev>
2020-12-21 13:19:23 -0300

    Added Concert entity

---
+++ lib/features/weather/domain/entities/concert.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta.dart';
+import 'package:rock_weather/features/weather/domain/entities/city.dart';
+
+class Concert {
+  final City city;
+
+  const Concert({@required this.city});
+}
